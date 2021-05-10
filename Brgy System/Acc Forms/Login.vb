73aedca4a55edf954d455af2a03ae6bb681414fe
PuriJ <jpurisima320@gmail.com>
2021-05-10 22:27:43 +0800

    Change Comment.

--- Brgy System/Acc Forms/Login.vb
+++ Brgy System/Acc Forms/Login.vb
@@ -132,7 +132,7 @@ Public Class Login
     End Sub
 
     Private Sub btnExit_Click_1(sender As Object, e As EventArgs) Handles btnExit.Click
-        'Exit
+        'Exit form
         If MessageBox.Show("Do you want to logout?", "Login", MessageBoxButtons.YesNo, MessageBoxIcon.Information) = vbYes Then
             Me.Close()
 
